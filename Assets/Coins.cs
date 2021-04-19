bc9b015b9f381a448fd7260ed1cbd9ce928e0e54
ninjechka <lizko170@gmail.com>
2021-04-19 13:33:22 +0500

    no message

---
+++ Assets/Coins.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour
+{
+    public int Cost;
+
+    void OnTriggerEnter2D(Collider2D col) { 
+        CoinsPlayer.money += Cost; 
+        GameObject.FindGameObjectWithTag ("Player").GetComponent<CoinsPlayer> ().TextMoney.text = CoinsPlayer.money.ToString(); 
+        Destroy (gameObject); 
+    }
+}
